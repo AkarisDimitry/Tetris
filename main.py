d231f7c9b54d31bb53c0209609673ab090013442
Chunchunmaru <34775621+AkarisDimitry@users.noreply.github.com>
2023-08-17 23:41:38 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ import random
 pygame.init()
 
 # Set the window dimensions
-window_width = 300
-window_height = 600
+window_width = 600
+window_height = 900
 
 # Create the window
 window = pygame.display.set_mode((window_width, window_height))
@@ -44,7 +44,7 @@ font = pygame.font.SysFont("Arial", 24)
 
 
 # Define DAS variables
-initial_delay = 500  # 500 milliseconds
+initial_delay = 100  # 500 milliseconds
 auto_repeat_rate = 50
 
 last_left_movement = 0
@@ -59,12 +59,19 @@ das_left_active = False
 das_right_active = False
 das_down_active = False
 
+# Define a dictionary to keep track of the statistics:
+statistics = {
+    "total_tetrominoes": 0,
+    "tetromino_counts": {"I": 0, "O": 0, "T": 0, "S": 0, "Z": 0, "J": 0, "L": 0},
+    "line_clears": {"single": 0, "double": 0, "triple": 0, "quadruple": 0}
+}
+
 # Define the colors
 colors = {
     "cyan": (0, 255, 255),
     "yellow": (255, 255, 0),
     "purple": (128, 0, 128),
-    "green": (0, 128, 0),
+    "green": (0, 128, 0 ),
     "red": (255, 0, 0),
     "blue": (0, 0, 255),
     "orange": (255, 165, 0),
@@ -196,15 +203,24 @@ wall_kick_offsets = {
 # Define a function to check for full lines and clear them
 def clear_lines(grid):
     cleared_lines = 0
-    new_grid = []
-    for row in grid:
-        if all(row!=0):
+    for i in range(grid_height):
+        if all(grid[i]):
             cleared_lines += 1
-        else:
-            new_grid.append(row)
-    for _ in range(cleared_lines):
-        new_grid.insert(0, [0] * grid_width)
-    return np.array(new_grid), cleared_lines
+            for j in range(i, 0, -1):
+                grid[j] = grid[j - 1]
+            grid[0] = [0] * grid_width
+
+    # Update statistics
+    if cleared_lines == 1:
+        statistics["line_clears"]["single"] += 1
+    elif cleared_lines == 2:
+        statistics["line_clears"]["double"] += 1
+    elif cleared_lines == 3:
+        statistics["line_clears"]["triple"] += 1
+    elif cleared_lines == 4:
+        statistics["line_clears"]["quadruple"] += 1
+
+    return grid, cleared_lines
 
 # Define a function to add the current tetromino to the game grid
 def add_to_grid(tetromino, current_tetromino_key, grid, position):
@@ -247,6 +263,35 @@ def calculate_shadow_position(tetromino, grid, position):
         shadow_position = (shadow_position[0] + 1, shadow_position[1])
     return shadow_position
 
+def get_tetromino_from_bag(bag):
+    if not bag:
+        # Refill the bag with all seven tetrominoes
+        bag.extend(['I', 'O', 'T', 'S', 'Z', 'J', 'L'])
+    # Draw a random tetromino from the bag
+    tetromino = random.choice(bag)
+    bag.remove(tetromino)
+    # Update statistics
+    statistics["total_tetrominoes"] += 1
+    statistics["tetromino_counts"][tetromino] += 1
+
+    return tetromino
+
+# function to lock the tetromino
+def lock_tetromino(current_tetromino, current_tetromino_key, current_position, grid, bag):
+    # Lock the tetromino in place
+    grid = add_to_grid(current_tetromino, current_tetromino_key, grid, current_position)
+    # Clear completed lines
+    grid, cleared_lines = clear_lines(grid)
+    score += cleared_lines
+    # Get the next tetromino from the bag
+    next_tetromino_key = get_tetromino_from_bag(bag)
+    next_tetromino = tetrominoes[next_tetromino_key]
+    current_position = (0, grid_width // 2 - 2)
+    if not is_valid_position(next_tetromino["shape"], grid, current_position):
+        return None  # Game over
+    hold_used = False  # Reset the hold action tracking for the next turn
+    return next_tetromino_key, next_tetromino, current_position
+
 # Test apply_wall_kick function
 apply_wall_kick(tetrominoes['T']["shape"], "T", "clockwise", "0->R", grid, (0, 0))
 
@@ -265,6 +310,11 @@ current_tetromino_key = random.choice(list(tetrominoes.keys()))
 current_tetromino = tetrominoes[current_tetromino_key]["shape"]
 current_position = (0, grid_width // 2 - 2)
 
+# Define hold system variables
+hold_tetromino_key = None
+hold_tetromino = None
+hold_used = False
+
 # Main game loop
 running = True
 
@@ -276,6 +326,13 @@ last_fall_time = pygame.time.get_ticks()
 
 # Initialize the rotation state
 rotation_state = "0"
+
+# Initialize an empty bag
+bag = []
+
+# Define a variable to store the start time:
+start_time = pygame.time.get_ticks()
+
 
 while running:
 
@@ -289,7 +346,34 @@ while running:
                 # Hard drop the tetromino
                 while is_valid_position(current_tetromino, grid, (current_position[0] + 1, current_position[1])):
                     current_position = (current_position[0] + 1, current_position[1])
-                    
+                # Lock the tetromino in place
+                grid = add_to_grid(current_tetromino, current_tetromino_key, grid, current_position)
+                # Clear completed lines
+                grid, cleared_lines = clear_lines(grid)
+                score += cleared_lines
+                # Spawn a new tetromino
+                current_tetromino_key = next_tetromino_key
+                current_tetromino = next_tetromino["shape"]
+                next_tetromino_key = get_tetromino_from_bag(bag)
+                next_tetromino = tetrominoes[next_tetromino_key]
+                current_position = (0, grid_width // 2 - 2)
+                if not is_valid_position(current_tetromino, grid, current_position):
+                    running = False  # Game over
+                hold_used = False  # Reset the hold action tracking for the next turn
+
+            elif event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                # Handle the hold action
+                if not hold_used:
+                    if hold_tetromino_key is None:
+                        hold_tetromino_key = current_tetromino_key
+                        current_tetromino_key = next_tetromino_key
+                        next_tetromino_key = get_tetromino_from_bag(bag)
+                    else:
+                        hold_tetromino_key, current_tetromino_key = current_tetromino_key, hold_tetromino_key
+                    current_tetromino = tetrominoes[current_tetromino_key]["shape"]
+                    current_position = (0, grid_width // 2 - 2)
+                    hold_used = True
+                        
             elif event.key == pygame.K_z or event.key == pygame.K_UP:
                 # Rotate the tetromino counter-clockwise
                 new_rotation_state = "L" if rotation_state == "0" else "2" if rotation_state == "R" else "R" if rotation_state == "2" else "0"
@@ -383,19 +467,22 @@ while running:
             score += cleared_lines
             current_tetromino_key = next_tetromino_key
             current_tetromino = next_tetromino["shape"]
-            next_tetromino_key = random.choice(list(tetrominoes.keys()))
+            next_tetromino_key = get_tetromino_from_bag(bag)
             next_tetromino = tetrominoes[next_tetromino_key]
             current_position = (0, grid_width // 2 - 2)
             if not is_valid_position(current_tetromino, grid, current_position):
                 running = False  # Game over
+            hold_used = False  # Reset the hold action tracking for the next turn
+        
         last_fall_time = current_time
 
+    grid_side = 30
     # Draw the game grid
     window.fill(colors["black"])
     for i in range(grid_height):
         for j in range(grid_width):
             color = colors[colors_list[grid[i][j]-1]] if grid[i][j] != 0 else colors["black"]
-            pygame.draw.rect(window, color, (j * 30, i * 30, 30, 30), 8)
+            pygame.draw.rect(window, color, (j*grid_side, i*grid_side, grid_side-1, grid_side-1), 8)
 
     # Calculate the shadow position
     shadow_position = calculate_shadow_position(current_tetromino, grid, current_position)
@@ -404,34 +491,73 @@ while running:
     for i in range(4):
         for j in range(4):
             if current_tetromino[i, j] == 1:
-                x = (shadow_position[1] + j) * 30
-                y = (shadow_position[0] + i) * 30
-                pygame.draw.rect(window, colors["grey"], (x, y, 30, 30))
-                pygame.draw.rect(window, colors["white"], (x, y, 30, 30), 1)
+                x = (shadow_position[1] + j) * grid_side
+                y = (shadow_position[0] + i) * grid_side
+                pygame.draw.rect(window, colors["grey"], (x, y, grid_side, grid_side))
+                pygame.draw.rect(window, colors["white"], (x, y, grid_side, grid_side), 1)
 
     # Display the current tetromino
     for i in range(4):
         for j in range(4):
             if current_tetromino[i, j] == 1:
                 color = colors[tetrominoes[current_tetromino_key]["color"]]
-                pygame.draw.rect(window, color, ((current_position[1] + j) * 30, (current_position[0] + i) * 30, 30, 30))
-                pygame.draw.rect(window, colors["white"], ((current_position[1] + j) * 30, (current_position[0] + i) * 30, 30, 30), 1)
+                pygame.draw.rect(window, color, ((current_position[1]+j)*grid_side, (current_position[0]+i)*grid_side, grid_side, grid_side))
+                pygame.draw.rect(window, colors["white"], ((current_position[1] + j)*grid_side, (current_position[0] + i)*grid_side, grid_side, grid_side), 1)
 
     # Display the next tetromino
     window.fill(colors["black"], (window_width, 0, 120, 120))
-    pygame.draw.rect(window, colors["white"], (window_width, 0, 120, 120), 2)
+    pygame.draw.rect(window, colors["white"], (window_width-grid_side*7, 0, grid_side*6, grid_side*5), 2)
     for i in range(4):
         for j in range(4):
             if next_tetromino["shape"][i, j] == 1:
                 color = colors[next_tetromino["color"]]
-                pygame.draw.rect(window, color, (window_width + j * 30, i * 30, 30, 30))
-                pygame.draw.rect(window, colors["white"], (window_width + j * 30, i * 30, 30, 30), 1)
+                pygame.draw.rect(window, color, (window_width-grid_side*6+j*grid_side, i*grid_side, grid_side, grid_side))
+                pygame.draw.rect(window, colors["white"], (window_width-grid_side*6+j*grid_side, i*grid_side, grid_side, grid_side), 1)
+
+
+    # Display the hold tetromino
+    window.fill(colors["black"], (window_width-grid_side*7, grid_side*7, grid_side*6, grid_side*6))
+    pygame.draw.rect(window, colors["red"], (window_width-grid_side*7, grid_side*7, grid_side*6, grid_side*6), 2)
+    if hold_tetromino_key is not None:
+        hold_tetromino = tetrominoes[hold_tetromino_key]["shape"]
+        for i in range(4):
+            for j in range(4):
+                if hold_tetromino[i, j] == 1:
+                    color = colors[tetrominoes[hold_tetromino_key]["color"]]
+                    pygame.draw.rect(window, color, (window_width-grid_side*6+ j*grid_side, grid_side*7+i*grid_side, grid_side, grid_side))
+                    pygame.draw.rect(window, colors["white"], (window_width-grid_side*6+j*grid_side, grid_side*7+i*grid_side, grid_side, grid_side), 1)
+
 
     # Display the score
     score_text = f"Score: {score}"
     score_surface = font.render(score_text, True, colors["white"])
-    window.blit(score_surface, (10, window_height - 30))
+    window.blit(score_surface, (window_width-grid_side*7, window_height - grid_side))
 
+    # Display the statistics
+    stats_text = f"Total Tetrominoes: {statistics['total_tetrominoes']}"
+    stats_surface = font.render(stats_text, True, colors["white"])
+    window.blit(stats_surface, (10, window_height - 60))
+
+    for i, (tetromino, count) in enumerate(statistics["tetromino_counts"].items()):
+        tetromino_text = f"{tetromino}: {count}"
+        tetromino_surface = font.render(tetromino_text, True, colors["white"])
+        window.blit(tetromino_surface, (10, window_height - 90 - i * 30))
+
+    for i, (line_clear, count) in enumerate(statistics["line_clears"].items()):
+        line_clear_text = f"{line_clear.capitalize()}: {count}"
+        line_clear_surface = font.render(line_clear_text, True, colors["white"])
+        window.blit(line_clear_surface, (window_width - 150, window_height - 90 - i * 30))
+
+    # Calculate and display the TPS:
+    elapsed_time = (pygame.time.get_ticks() - start_time) / 1000
+
+    if elapsed_time > 0:
+        tps = statistics["total_tetrominoes"] / elapsed_time
+    else:
+        tps = 0
+    tps_text = f"TPS: {tps:.2f}"
+    tps_surface = font.render(tps_text, True, colors["white"])
+    window.blit(tps_surface, (window_width - 150, 10))
 
     pygame.display.flip()
 
